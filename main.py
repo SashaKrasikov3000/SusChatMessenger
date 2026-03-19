38ffbd2345ad90fede628f8c915ea57d23ff52f5
Sasha <krasikov.develop@gmail.com>
2026-03-19 23:14:05 +0300

    Added database manager object (first version)

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import sqlite3 as sqlt
 
 app = Flask("SusChat")
 
+class DatabaseManager:
+
+    def __init__(self):
+        self.con = sqlt.connect("database.db")
+        self.cursor = self.con.cursor()
+
+    def sql_query(self, query):
+        self.cursor.execute(query)
+        self.con.commit()
+        return self.cursor.fetchall()
+
+    def get_users(self):
+        return self.sql_query("SELECT * FROM users")
+
+    def add_user(self, name):
+        return self.sql_query(f"INSERT INTO users (name) VALUES ('{name}')")
+
 
 @app.route('/')
 def home_page():
